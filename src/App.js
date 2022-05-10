d0e61f51d9248f83f5be95c142775ba309290c6b
dong53358 <dong53358@naver.com>
2022-05-11 01:37:56 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App(event) {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [selecting, setSelecting] = useState(false);
+  const [coins, setCoins] = useState([]);
+  const [coinSymbol, setCoinSymbol] = useState("");
+  const [input, setInput] = useState("");
+  const [selectCoin, setSelectCoin] = useState("");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const onSelectCoin = (event) => {
+    setSelectCoin(event.target.value);
+    setCoinSymbol(event.target.title);
+    setSelecting(true);
+  };
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
   };
-  console.log(toDos);
+  console.log(coins);
   return (
     <div>
-      <h1>My To do({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelectCoin}>
+          <option>--Please choose Coin--</option>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {loading ? null : (
+        <form onSubmit={onSubmit}>
+          <input
+            value={input}
+            type="number"
+            onChange={onChange}
+            placeholder="Enter money"
+          />
+          <button>변환</button>
+          <h1>보유 머니 : {input} 원</h1>
+          <h1>구매가능 : {selecting ? input / (selectCoin * 1276) : 0} 개</h1>
+
+          <h1>원화 : {selecting ? selectCoin * 1276 : 0} 원</h1>
+        </form>
+      )}
     </div>
   );
 }
