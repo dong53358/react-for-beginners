9eba0f08cc3f261d8250fc1735eaf62f636d3411
dong53358 <dong53358@naver.com>
2022-05-12 15:45:41 +0900

    version 2

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
+    <div className={styles.main}>
+      <div className={styles.imgAndTitle}>
+        <img src={coverImg} alt={title} />
+        <h2>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
+      </div>
+      <div className={styles.summary}>
+        <p>{summary}</p>
+      </div>
     </div>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,49 @@
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./css/Detail.module.css";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
   const { id } = useParams();
-  const getMovie = async () => {
+  const getDetails = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setDetails(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
-    getMovie();
+    getDetails();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div className={styles.body}>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div className={styles.main}>
+          <div className={styles.imgAndTitle}>
+            <span className={styles.img}>
+              <img src={details.medium_cover_image} />
+            </span>
+            <span className={styles.title}>{details.title}</span>
+          </div>
+          <div className={styles.info}>
+            <span>{details.year}</span>
+            <span>{details.rating}★</span>
+            <span>{details.runtime}분</span>
+            <span>
+              {details.genres &&
+                details.genres.map((g) => <li key={g}>{g}</li>)}
+            </span>
+          </div>
+          <div className={styles.summary}>
+            <span>{details.description_intro}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
