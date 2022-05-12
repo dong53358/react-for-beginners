d3ff0ff65f59208b4a50ded83b9b23e34a707e02
dong53358 <dong53358@naver.com>
2022-05-13 00:09:35 +0900

    123

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/123" element={<h1>Hello!</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
